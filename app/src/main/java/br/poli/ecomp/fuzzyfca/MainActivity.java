13881761b815e0f207067290a870c74a6a14ce74
Paula Pithon <paulapithon@gmail.com>
2017-12-11 02:06:49 -0300

    Adicionar detalhes finais ao código em java

--- app/src/main/java/br/poli/ecomp/fuzzyfca/MainActivity.java
+++ app/src/main/java/br/poli/ecomp/fuzzyfca/MainActivity.java
@@ -1,23 +1,35 @@
 package br.poli.ecomp.fuzzyfca;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import net.sourceforge.jFuzzyLogic.FIS;
-import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
 import net.sourceforge.jFuzzyLogic.rule.Variable;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static int PERMISSION_STORAGE = 333;
+
     private TextView mSaida;
-    private EditText mProblema;
     private EditText mBuffer;
+    private Spinner mSpinner;
+
+    double problema;
+    double buffer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +37,117 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mSaida = findViewById(R.id.saida);
-        mProblema = findViewById(R.id.problema);
         mBuffer = findViewById(R.id.buffer);
+        mSpinner = findViewById(R.id.spinner);
     }
 
-    public void getFuzzyAnswer (View v) throws Exception {
+    public void getFuzzyAnswer(View v) {
 
-        //Carrega arquivo .flc
-        InputStream inputStream = getAssets().open("fuzzy.fcl");
-        FIS fis = FIS.load(inputStream, true);
-
-        if (fis != null) {
-            JFuzzyChart.get().chart(fis);
-
-            // Definir inputs
-            fis.setVariable("problema", Double.parseDouble(mProblema.getText().toString()));
-            fis.setVariable("buffer", Double.parseDouble(mBuffer.getText().toString()));
-            fis.evaluate();
-
-            // Mostra gráfico da saída
-            Variable tempo = fis.getVariable("tempo");
-            mSaida.setText(String.valueOf(tempo.getValue()));
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            loadFuzzy();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_STORAGE);
         }
 
-        Log.d("FUZZY", "A saída é: " + fis.getVariable("tempo").getValue());
+    }
+
+    private void loadFuzzy() {
+        //Carrega arquivo .flc
+        try {
+            InputStream inputStream = getAssets().open("fuzzy.fcl");
+            FIS fis = FIS.load(inputStream, true);
+
+            if (fis != null) {
+
+                getProblema();
+                // Definir inputs
+                fis.setVariable("problema", problema);
+                fis.setVariable("buffer", buffer);
+                fis.evaluate();
+
+                // Mostra gráfico da saída
+                Variable atividade = fis.getVariable("atividade");
+                mSaida.setText(Html.fromHtml(getResultado(atividade)));
+            }
+
+            Log.d("FUZZY", "A saída é: " + fis.getVariable("atividade").getValue());
+        } catch (IOException e) { e.printStackTrace(); }
+    }
+
+    private void getProblema() {
+        //Atribuir tempo aos problemas
+        String item = mSpinner.getSelectedItem().toString();
+        Resources resources = getResources();
+        if (item.equals(resources.getString(R.string.problema_aplicacao))) {
+            problema = 55.55;
+        } else if (item.equals(resources.getString(R.string.problema_automatico))) {
+            problema = 35.0;
+        } else if (item.equals(resources.getString(R.string.problema_eletrodo))) {
+            problema = 28.26;
+        } else if (item.equals(resources.getString(R.string.problema_falha))) {
+            problema = 29;
+        } else if (item.equals(resources.getString(R.string.problema_manual))) {
+            problema = 21.4;
+        } else if (item.equals(resources.getString(R.string.problema_pecas))) {
+            problema = 37.56;
+        } else if (item.equals(resources.getString(R.string.problema_sistema))) {
+            problema = 33.4;
+        } else {
+            problema = 0;
+        }
+        buffer = Double.parseDouble(mBuffer.getText().toString());
+    }
+
+    private String getResultado(Variable variable) {
+        double resultado = variable.getValue();
+
+        //Checar grau de certeza do resultado
+        int atividade, certeza;
+        double flutuante = resultado - (int) resultado;
+        if (flutuante > 0.5) {
+            atividade = (int) resultado + 1;
+            certeza = (int) (flutuante * 100);
+        } else {
+            atividade = (int) resultado;
+            certeza = 100 - (int) (flutuante * 100);
+        }
+
+        //Retorno final em html
+        String retorno = "Realizar atividade <b>";
+        switch (atividade) {
+            case 0:
+                retorno += "limpeza";
+                break;
+            case 1:
+                retorno += "inspeção";
+                break;
+            case 2:
+                retorno += "lubrificação";
+                break;
+            case 3:
+                retorno += "reaperto";
+                break;
+            case 4:
+                retorno += "nenhuma";
+                break;
+        }
+        //Tempo convertido em minutos
+        int tempo = (int) (problema / 60 + buffer * 0.75);
+        retorno += "</b> em <b>" + tempo + " minutos</b> com <b>" + certeza + "%</b> de sucesso.";
+
+        return retorno;
 
     }
 
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSION_STORAGE && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            loadFuzzy();
+        } else {
+            finish();
+        }
+    }
 }
